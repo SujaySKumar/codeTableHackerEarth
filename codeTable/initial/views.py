be4e584c3ef7212f13b979f4fd363d67b5b5301a
SujaySKumar <sujay.skumar141295@gmail.com>
2015-12-22 16:00:05 +0530

    Adds support for multiple languages

--- codeTable/initial/views.py
+++ codeTable/initial/views.py
@@ -17,12 +17,13 @@ def index(request):
 	
 	return HttpResponse(template.render())
 
-def sujay(code):
+def sujay(code, language):
 	client_secret = '0a7f0101e5cc06e4417a3addeb76164680ac83a4'
 
 	#source = open('test_source.py', 'r').read()
 	source=code
-	lang = SupportedLanguages.PYTHON
+	#lang = SupportedLanguages.PYTHON
+	lang = language
 	compressed = 1
 	html = 0
 	params = RunAPIParameters(
@@ -48,9 +49,10 @@ def sujay(code):
 @csrf_exempt
 def submitCode(request):
 	code = request.POST['code']
+	lang = request.POST['lang']
 	#print code
-	output = sujay(code)
-	template = loader.get_template('initial/results.html')
+	output = sujay(code, lang)
+	template = loader.get_template('initial/index.html')
 	context = RequestContext(request, {
         'output': output,
     })
